828da1ffb26f8436d409dce672ed2d3d50b0e559
cellolian <akatsuki831@yahoo.co.jp>
2020-08-04 11:46:13 +0900

    add beep

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from putSerial import PutSerial
 import argparse
 import time
 import random
+import platform
+
+def beep(freq, dur=100):
+    """
+        ビープ音を鳴らす.
+        @param freq 周波数
+        @param dur  継続時間（ms）
+    """
+    if platform.system() == "Windows":
+        # Windowsの場合は、winsoundというPython標準ライブラリを使います.
+        import winsound
+        winsound.Beep(freq, dur)
+    else:
+        # Macの場合には、Macに標準インストールされたplayコマンドを使います.
+        import os
+        os.system('play -n synth %s sin %s' % (dur/1000, freq))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -28,6 +44,9 @@ def main():
         chats = gc.get()
         # print(len(chats))
         for chat in chats:
+            if zero_chat_counter > 60 * 2:
+                beep(2000)
+                beep(2000)
             zero_chat_counter = 0
             text = chat[1]
             # typeKey.press_key(text)
@@ -39,7 +58,7 @@ def main():
             if zero_chat_counter > 60 * 5:
                 ps.press_key('b')
                 rand_direction = random.choice(['2', '4', '6', '8'])
-                ps.press_key(rand_direction * 5)
+                ps.press_key(rand_direction * 3)
                 ps.press_key('y')
 
 if __name__ == '__main__':
